09e884120b096e67548ffbefb5759d4187e7d7c7
Sid Patel <sidpatel13@gmail.com>
2014-05-06 00:02:43 -0400

    boggle_board

--- week_5/4_boggle_board/my_solution.rb
+++ week_5/4_boggle_board/my_solution.rb
@@ -2,17 +2,57 @@
 
 # I worked on this challenge [by myself, with: ].
 
-boggle_board = [["b", "r", "a", "e"],
-                ["i", "o", "d", "t"],
-                ["e", "c", "l", "r"],
-                ["t", "a", "k", "e"]]
+        #board = [["b", "r", "a", "e"],
+                #["i", "o", "d", "t"],
+                #["e", "c", "l", "r"],
+                #["t", "a", "k", "e"]]
 
 
 # Part 1: Access multiple elements of a nested array
 
+#def create_word(board, *coords)
+#coords.map { |coord| board[coord.first][coord.last]}.join("")
+#end
+
+#puts create_word(boggle_board, [2,1], [1,1], [1,2], [0,3])
+#puts create_word(boggle_board, [0,1], [0,2], [1,2])
+
+#def get_row(row)
+    # your code here
+#end
+
+#get_row(1)
+
+#def get_col(col)
+    # your code here
+#end
+
+#get_col(1)
+
+
+
 # Pseudocode
 
 # Initial Solution
+class Boggle_Board
+    
+    def initialize(board)
+        @board = board
+    end
+    
+    def create_word(*coords)
+        coords.map { |coord| @board[coord.first][coord.last]}.join("")
+    end
+    
+    def get_row(row)
+        @board[row].join(",")
+    end
+    
+    def get_column(column)
+        @board.map {|row| row[column]}.join(",")
+    end
+    
+ end
 
 
 # Refactored Solution
@@ -20,8 +60,22 @@ boggle_board = [["b", "r", "a", "e"],
 
 # DRIVER TESTS GO BELOW THIS LINE
 
+board = [["b", "r", "a", "e"],
+["i", "o", "d", "t"],
+["e", "c", "l", "r"],
+["t", "a", "k", "e"]]
 
-# Reflection 
+board = Boggle_Board.new(board)
+
+
+puts board.create_word([2,1], [1,1], [1,2], [0,3])
+puts board.get_row(1)
+puts board.get_column(2)
+
+
+
+
+# Reflection
 
 #-------------------------------------------------------------------------------
 
