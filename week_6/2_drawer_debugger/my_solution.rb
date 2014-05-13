6ce87691f63c7ca29aaeef4e0389dedf973fe405
Sid Patel <sidpatel13@gmail.com>
2014-05-13 16:14:06 -0400

    drawer_debugger changes

--- week_6/2_drawer_debugger/my_solution.rb
+++ week_6/2_drawer_debugger/my_solution.rb
@@ -1,4 +1,4 @@
-]
+
 # U2.W6: Drawer Debugger
 
 
@@ -13,53 +13,61 @@ attr_reader :contents
 
 # Are there any more methods needed in this class?
 
-def initialize
-@contents = []
-@open = true
+  def initialize
+    @contents = []
+    @open = true
+  end
+
+  def open
+    @open = true
+  end
+
+  def close
+    @open = false
+  end 
+
+  def add_item(item)
+    @contents << item
+  end
+
+  def remove_item(item = @contents.pop) #what is `#pop` doing? removing the item from the list
+     @contents.delete(item)
+  end
+
+
+  def dump  # what should this method return? statement your drawer is empty
+    puts "Your drawer is empty."
+  end
+
+  def view_contents
+    puts "The drawer contains:"
+    @contents.each {|silverware| puts "- " + silverware.type }
+  end
+
 end
-
-def open
-@open = true
-end
-
-def close
-@open = false
-end 
-
-def add_item
-@contents << item
-end
-
-def remove_item(item = @contents.pop) #what is `#pop` doing?
-@contents.delete(item)
-end
-
-def dump  # what should this method return?
-puts "Your drawer is empty."
-end
-
-def view_contents
-puts "The drawer contains:"
-@contents.each {|silverware| puts "- " + silverware.type }
-end
-
 
 class Silverware
 attr_reader :type
 
-# Are there any more methods needed in this class?
+# Are there any more methods needed in this class? yes a clean_silverware method
 
-def initialize(type, clean = true)
-@type = type
-@clean = clean
+  def initialize(type, clean = true)
+    @type = type
+    @clean = clean
+  end
+
+  def eat
+    puts "eating with the #{type}"
+    @clean = false
+  end
+
+
+  def clean_silverware
+    
+  end
+
 end
 
-def eat
-puts "eating with the #{type}"
-@clean = false
-end
-
-end
 
 knife1 = Silverware.new("knife")
 
@@ -84,11 +92,14 @@ removed_knife.clean_silverware
 
 silverware_drawer.view_contents
 silverware_drawer.dump
-silverware_drawer.view_contents #What should this return?
+silverware_drawer.view_contents #What should this return? Nothing since we dumped all of the items
 
+fork= Silverware.new('fork')
+silverware_drawer.add_item(fork)
+silverware_drawer.view_contents
 
-fork = silverware_drawer.remove_item(fork) #add some puts statements to help you trace through the code...
-fork.eat
+remove_fork = silverware_drawer.remove_item(fork) #add some puts statements to help you trace through the code...
+remove_fork.eat
 
 #BONUS SECTION
 # puts fork.clean
